fceefbd5635969187b4c4146cb649178f5f194be
mbarger <matthewbarger2@gmail.com>
2021-12-11 14:28:09 -0500

    final save

--- aggregation_revisited.sql
+++ aggregation_revisited.sql
@@ -3,16 +3,20 @@ Use sakila;
 
 #1) Select the first name, last name, and email address of all the
 # customers who have rented a movie.
-select f.email, concat(f.first_name,' ', f.last_name) as full_name from sakila.payment e 
+select  f.email, concat(f.first_name,' ', f.last_name) as full_name from sakila.payment e 
+join sakila.customer f on f.customer_id=e.customer_id
+group by full_name, email;
+
+select distinct f.email, concat(f.first_name,' ', f.last_name) as full_name from sakila.payment e 
 join sakila.customer f on f.customer_id=e.customer_id;
-
-
 
 #2) What is the average payment made by each customer 
 #(display the customer id, customer name (concatenated), and the average payment made).
 
 select f.customer_id, concat(f.first_name,' ', f.last_name) as full_name, round(avg(e.amount),0) as avg_pay
- from  sakila.payment e join sakila.customer f on f.customer_id=e.customer_id;
+ from  sakila.payment e 
+ join sakila.customer f on f.customer_id=e.customer_id
+ group by customer_id;
 
 
 
@@ -21,13 +25,20 @@ select f.customer_id, concat(f.first_name,' ', f.last_name) as full_name, round(
 # Write the query using sub queries with multiple WHERE clause and IN condition
 # Verify if the above two queries produce the same results or not
 
-select f.email, concat(f.first_name,' ', f.last_name) as full_name from sakila.category a
+select  f.email, concat(f.first_name,' ', f.last_name) as full_name from sakila.category a
 join sakila.film_category b using (category_id)
 join sakila.inventory c using (film_id)
 join sakila.rental d using (inventory_id)
 join sakila.payment e using (rental_id)
 join sakila.customer f on f.customer_id=e.customer_id
 where a.name = 'Action';
+
+select email, concat(first_name,' ', last_name) as full_name
+from customer where customer_id in 
+(select customer_id from rental where inventory_id in 
+(select inventory_id from inventory where film_id in
+(select film_id from film_category join category using (category_id)
+where category.name ="Action")));
 
 #4) Use the case statement to create a new column classifying existing columns as 
 # either or high value transactions based on the amount of payment. 
@@ -36,7 +47,7 @@ where a.name = 'Action';
 # and if it is more than 4, then it should be high.
 
 Select *,
-case when amount >0 and amount <=2 then 'low'
+case when amount >=0 and amount <=2 then 'low'
 when amount >2 and amount <=4 then 'medium'
 when amount >4 then 'high'
 end as trans_amount
